5424bc9b9197cb8d85ef7397e42c772405b79fb0
ParkDogeon <bdg3126@gmail.com>
2023-05-15 12:59:19 +0900

    프론트 레이아웃 템플릿 완성

--- src/main/java/org/dogun/configs/MvcConfig.java
+++ src/main/java/org/dogun/configs/MvcConfig.java
@@ -1,16 +1,23 @@
 package org.dogun.configs;
 
+import lombok.RequiredArgsConstructor;
+import org.dogun.configs.interceptors.SiteConfigInterceptor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
+@RequiredArgsConstructor
 public class MvcConfig implements WebMvcConfigurer {
 
     @Value("${file.upload.path}")
     private String fileUploadPath;
+
+    // 사이트 설정 유지 인터셉터
+    private final SiteConfigInterceptor siteConfigInterceptor;
 
     @Override
     // main의 동작을 확인하기 위한 임시용이다.
@@ -23,5 +30,11 @@ public class MvcConfig implements WebMvcConfigurer {
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/uploads/**")
                 .addResourceLocations("file:///" + fileUploadPath);
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(siteConfigInterceptor)
+                .addPathPatterns("/**");
     }
 }
