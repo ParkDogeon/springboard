553e5a531e679c20af749eb42c6e23d0eac62ad1
Sseryou <tjdalstn11@gmail.com>
2023-05-15 10:59:03 +0900

    스프링 설정

---
+++ src/test/java/org/dogun/BoardApplicationTests.java
@@ -0,0 +1,13 @@
+package org.dogun;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
