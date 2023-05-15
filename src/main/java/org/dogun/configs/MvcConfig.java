921b16c4332055eb32ef01a15cab059ef630698b
ParkDogeon <bdg3126@gmail.com>
2023-05-15 11:55:53 +0900

    스프링 시큐리티 기본 설정, 정적 경로 설정

---
+++ src/main/java/org/dogun/configs/MvcConfig.java
@@ -0,0 +1,27 @@
+package org.dogun.configs;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class MvcConfig implements WebMvcConfigurer {
+
+    @Value("${file.upload.path}")
+    private String fileUploadPath;
+
+    @Override
+    // main의 동작을 확인하기 위한 임시용이다.
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/")
+                .setViewName("main/index");
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/uploads/**")
+                .addResourceLocations("file:///" + fileUploadPath);
+    }
+}
